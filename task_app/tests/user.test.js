36739390dbae6921f185025fd6526cfafad8a408
lizzochek <littled685@gmail.com>
2021-07-25 20:12:05 +0300

    User updating tests

--- task_app/tests/user.test.js
+++ task_app/tests/user.test.js
@@ -102,3 +102,40 @@ test("Should delete an account", async () => {
 test("Should not delete profile for not authenticated user", async () => {
   await request(app).delete("/users/me").send().expect(401);
 });
+
+test("Should upload avatar image", async () => {
+  await request(app)
+    .post("/users/me/avatar")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .attach("avatar", "tests/fixtures/profile-pic.jpg")
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test("Should update valid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: "Jessica",
+    })
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.name).toEqual("Jessica");
+});
+
+test("Should not update invalid fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      home: "Kyiv",
+    })
+    .expect(400);
+
+  const user = await User.findById(userOneId);
+  expect(user.name).toEqual("Anna");
+});
