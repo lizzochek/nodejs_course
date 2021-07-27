6608b15c17bdf350d25760e6cec207d569decb97
lizzochek <littled685@gmail.com>
2021-07-27 13:47:13 +0300

    Chat app initial commit

---
+++ chat_app/src/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const path = require("path");
+const port = process.env.PORT || 3000;
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", (socket) => {
+  console.log("New user connected");
+
+  socket.emit("message", "Welcome to the chat app!");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
