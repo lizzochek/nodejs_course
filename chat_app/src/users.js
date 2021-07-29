f4db7c18ef129def6e4296bdc22f65d7cbd75b0e
lizzochek <littled685@gmail.com>
2021-07-29 13:09:33 +0300

    Helper functions for working with users

---
+++ chat_app/src/users.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const users = [];
+
+const addUser = ({ id, username, room }) => {
+  //Check fields
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  if (!room || !username) return { error: "Username and room are required!" };
+
+  //Check user existance
+  const userExists = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
+
+  if (userExists) return { error: "Username is already used" };
+
+  //Store user
+  const user = { id, username, room };
+  users.push(user);
+  return user;
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) return users.splice(index, 1);
+};
+
+const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+const getUsersInRoom = (room) => {
+  return users.filter((user) => user.room === room);
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+};
